1597f6cf8368ab8dc65850aff6ae1ab91e43b629
Andy Tan <andyl7an@gmail.com>
2020-03-18 22:11:08 -0700

    Remove extraneous quotes

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -3,7 +3,7 @@ const { resolve } = require('path')
 
 // Create the dotenv secrets for bundling
 const secrets = ['DIALOGFLOW_PRIVATE_KEY', 'DIALOGFLOW_PRIVATE_EMAIL']
-const dotenvContent = secrets.map(s => `${s}="${process.env[s]}"`).join('\n')
+const dotenvContent = secrets.map(s => `${s}=${process.env[s]}`).join('\n')
 const dotenvLocation = resolve(__dirname, '../', `.env.production`)
 console.log(`Writing ${dotenvLocation}`)
 console.log(dotenvContent);
